80926acf8408453e77282d287e7c2777e4b32f4e
Andreas <andrn19@student.sdu.dk>
2022-03-28 21:53:24 +0200

    Click outside dropdown closes it

--- blockly-react-test/src/Components/ChooseMethod.jsx
+++ blockly-react-test/src/Components/ChooseMethod.jsx
@@ -1,27 +1,49 @@
-import React, { useState } from "react";
-import { DropdownButton, Dropdown } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import { useEffect } from "react";
 
 import "./DropdownBtnStyle.css";
+
+const useClickOutside = (handler) => {
+    const domNode = useRef();
+
+    useEffect(() => {
+        const maybeHandler = (event) => {
+            if (!domNode.current.contains(event.target)) {
+                handler();
+            };
+        };
+        document.addEventListener("mousedown", maybeHandler);
+        return () => {
+            document.removeEventListener("mousedown", maybeHandler);
+        };
+    });
+    return domNode
+};
 
 const ChooseMethod = () => {
 
     const [isActive, setIsActive] = useState(false);
     const [value, setValue] = useState('Methods');
     const setSelected = (e) => {
-        setValue(e)
-    }
+        setValue(e);
+    };
     const options = ["Clean", "Clear", "Get that pepper off there"];
 
-
     const addMethod = () => {
-        setValue("Methods")
-    }
+        setValue("Methods");
+    };
+
+    const domNode = useClickOutside(() => {
+        setIsActive(false);
+    });
+
+
 
     return (
         <div id="ChooseMethodDiv">
             <h2>Clearing Methods</h2>
             <p>Step 2: Choose methods for selected tables</p>
-            <div className="dropdown">
+            <div ref={domNode} className="dropdown">
                 <div className="dropdown-btn" onClick={(e) =>
                     setIsActive(!isActive)}>
                     {value}
@@ -29,7 +51,7 @@ const ChooseMethod = () => {
                 </div>
                 {isActive && (
                     <div className="dropdown-content">
-                        {options.map((option) => 
+                        {options.map((option) =>
                             <div
                                 onClick={(e) => {
                                     setSelected(option);
